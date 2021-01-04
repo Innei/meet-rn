39b220f94b074f9d3e0cd4381e42234f37dede94
Innei <tukon479@gmail.com>
2021-01-04 20:23:42 +0800

    chore: update api url

---
+++ configs.ts
@@ -0,0 +1,3 @@
+export const configs = Object.freeze({
+  api_url: 'https://api.innei.ren/hikotoko/v1',
+});
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import camelcaseKeys from 'camelcase-keys';
+import { configs } from '../../configs';
 import { UserStore } from '../store/user';
 import { JSONParseWithDate } from './json-parser';
 /**
  * @see https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
  */
 export const $http = axios.create({
-  baseURL: 'http://127.0.0.1:3000/v1',
+  baseURL: configs.api_url,
   transformResponse: (res) => {
     return JSONParseWithDate(res);
   },
